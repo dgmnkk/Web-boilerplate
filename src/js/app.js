c2e60e18f7ed37d845fd6f808967ea8ec58779dc
dgmnkk <125593353+dgmnkk@users.noreply.github.com>
2024-09-16 18:20:31 +0300

    html and css of project done. practice 1

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,32 @@ const testModules = require('./test-module');
 require('../css/app.css');
 
 /** ******** Your code here! *********** */
+const modal = document.getElementById('modal');
+const openModalBtn = document.getElementById('add-teacher');
+const closeModalBtn = document.querySelector('.close-btn');
 
+// Відкриття модального вікна при натисканні на кнопку
+openModalBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+});
+
+// Закриття модального вікна при натисканні на кнопку закриття
+closeModalBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+const teacherCard = document.getElementById('teacherCard1');
+const teacherModal = document.getElementById('teacherModal');
+const closeTeacherModalBtn = document.querySelector('.close-btn-teacherModal');
+
+// Відкриття модального вікна при натисканні на картку
+teacherCard.addEventListener('click', () => {
+    teacherModal.style.display = 'flex';
+});
+
+// Закриття модального вікна при натисканні на кнопку закриття
+closeTeacherModalBtn.addEventListener('click', () => {
+    teacherModal.style.display = 'none';
+});
 console.log(testModules.hello);
+
